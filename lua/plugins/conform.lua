685f3d45467be55ee2dec9cf985d24f368bb5d80
PhyTensor <karoki.tphy@gmail.com>
2026-08-29 01:38:34 +0300

    typescript and javascript conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,5 +2,9 @@ require('conform').setup({
     formatters_by_ft = {
         -- python = { "ruff" },
         python = { "ruff", "ruff_format", "ruff_organize_imports" },
+		javascript = { "prettier", "prettierd" },
+		typescript = { "prettier", "prettierd" },
+		javascriptreact = { "prettier", "prettierd" },
+		typescriptreact = { "prettier", "prettierd" },
     }
 })
